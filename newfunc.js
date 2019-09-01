fa15f7460a05bccf7a8dd8d63b0aa960a2207b9b
Rachinda <Rachinda@users.noreply.github.com>
2019-09-01 19:25:23 +0530

    Update newfunc.js

--- newfunc.js
+++ newfunc.js
@@ -1,20 +1,3 @@
-	var close1 = document.getElementById('Image1');
-	setInterval(
-		function(){ 
-			if(close1){
-				console.clear();
-				close1.click();
-				console.log("Click Close");
-			}
-		}, 1000);
-
-
-	var next1 = document.getElementById('submit3');
-	setInterval(
-		function(){ 
-			if(next1){
-				console.clear();
-				next1.click();
-				console.log("Click Next");
-			}
-		}, 1000);
+(function(){
+alert("Test");
+})();
